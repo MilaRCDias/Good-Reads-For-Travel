2be30d345d1898782a9ba30da04c7c25fdc228b5
mila <milarabello.dias@gmail.com>
2020-10-22 23:56:02 +0100

    animation for empty state

---
+++ src/containers/EmptyState/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Lottie from 'react-lottie';
+import animation from '../../assets/animation/reading-clouds.json';
+
+
+
+/**
+ * Component with animation to show on empty state
+ */
+ const EmptyState = () => {
+  
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+    return (
+    <div>
+        <Lottie options={defaultOptions}
+            width={300}
+         />
+    </div>
+  );
+};
+
+export default EmptyState;
